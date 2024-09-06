1f7cbed01b29c70b19025268f32912a24d36c7f0
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-09-06 09:30:06 -0400

    Update debounce and content.js

--- content.js
+++ content.js
@@ -1,19 +1,32 @@
-/* Event Listener that selects highlighted text and sends to Background.js*/
 
-document.addEventListener('mouseup', function(event) {
-  var selectedText = window.getSelection().toString();
-  if (selectedText.length > 0) {
+const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
 
-    chrome.runtime.sendMessage({ message: 'getSelectedText', selectedText: selectedText }, function(response) {
+const sendSelectedText = debounce((selectedText) => {
+  if (chrome.runtime && chrome.runtime.sendMessage) {
+    chrome.runtime.sendMessage({ message: 'getSelectedText', selectedText }, (response) => {
       if (chrome.runtime.lastError) {
         console.error('Error in sendMessage:', chrome.runtime.lastError);
-      } else {
-        console.log('Response from background script:', response);
       }
+      // Remove or comment out the following line in production
+      // console.log('Response from background script:', response);
     });
+  } else {
+    console.error('Chrome runtime not available');
+  }
+}, 300);
+
+document.addEventListener('mouseup', () => {
+  const selectedText = window.getSelection().toString().trim();
+  if (selectedText.length > 0) {
+    sendSelectedText(selectedText);
   }
 });
-
 
 
 
